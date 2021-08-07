ee7322fc91ee7a19161af702008049c003b2940e
Juan Carlos Marino Morantes <juan.marino.morantes@gmail.com>
2021-08-07 13:47:22 -0500

    GUI

---
+++ Proyecto con GUI/Music/src/main/java/Controlador/Controller.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Modelo.Clases.Cancion;
+import Modelo.Persistencia.CRUD;
+import Modelo.Persistencia.DbConnection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author JUAN MARIÑO
+ */
+public abstract class Controller {
+
+    public static boolean registrarCancion(String nombre, String artista, String genero) throws SQLException {
+        Cancion c1 = new Cancion(0, nombre, artista, genero);
+        CRUD.setConnection(DbConnection.ConexionBD());
+        String sentencia = "INSERT INTO Cancion(nombre,artista,genero) "
+                + " VALUES ('" + c1.getNombre() + "','" + c1.getArtista() + "','" + c1.getGenero() + "');";
+        if (CRUD.setAutoCommitBD(false)) {
+            if (CRUD.insertarBD(sentencia)) {
+                CRUD.commitBD();
+                CRUD.cerrarConexion();
+                return true;
+            } else {
+                CRUD.rollbackBD();
+                CRUD.cerrarConexion();
+                return false;
+            }
+        } else {
+            CRUD.cerrarConexion();
+            return false;
+        }
+    }
+
+    public static boolean actualizarCancion(int id, String nombre, String artista, String genero) {
+        Cancion c1 = new Cancion(id, nombre, artista, genero);
+        CRUD.setConnection(DbConnection.ConexionBD());
+        String Sentencia = "UPDATE `Cancion` SET nombre='" + c1.getNombre() + "',artista='" + c1.getArtista() + "',genero='" + c1.getGenero()
+                + "' WHERE id=" + c1.getID() + ";";
+        if (CRUD.setAutoCommitBD(false)) {
+            if (CRUD.actualizarBD(Sentencia)) {
+                CRUD.commitBD();
+                CRUD.cerrarConexion();
+                return true;
+            } else {
+                CRUD.rollbackBD();
+                CRUD.cerrarConexion();
+                return false;
+            }
+        } else {
+            CRUD.cerrarConexion();
+            return false;
+        }
+    }
+
+    public static boolean borrarCancion(int id) {
+        CRUD.setConnection(DbConnection.ConexionBD());
+        String Sentencia = "DELETE FROM `Cancion` WHERE `id`='" + id + "';";
+        if (CRUD.setAutoCommitBD(false)) {
+            if (CRUD.borrarBD(Sentencia)) {
+                CRUD.commitBD();
+                CRUD.cerrarConexion();
+                return true;
+            } else {
+                CRUD.rollbackBD();
+                CRUD.cerrarConexion();
+                return false;
+            }
+        } else {
+            CRUD.cerrarConexion();
+            return false;
+        }
+    }
+
+    public static Cancion obtenerCancion(int id) {
+        CRUD.setConnection(DbConnection.ConexionBD());
+        String sql = "select * from Cancion where id=" + id + ";";
+        ResultSet rs = CRUD.consultarBD(sql);
+        Cancion c1 = new Cancion();
+        try {
+            if (rs.next()) {
+                c1.setID(rs.getInt("ID"));
+                c1.setNombre(rs.getString("nombre"));
+                c1.setArtista(rs.getString("artista"));
+                c1.setGenero(rs.getString("genero"));
+
+                CRUD.cerrarConexion();
+            } else {
+                CRUD.cerrarConexion();
+                return null;
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+        return c1;
+    }
+
+    public static List<Cancion> listarCanciones() {
+        CRUD.setConnection(DbConnection.ConexionBD());
+        List<Cancion> listaCanciones = new ArrayList<>();
+        try {
+            String sql = "select * from Cancion";
+
+            ResultSet rs = CRUD.consultarBD(sql);
+            
+            while (rs.next()) {
+                Cancion c1 = new Cancion();
+                c1.setID(rs.getInt("ID"));
+                c1.setNombre(rs.getString("nombre"));
+                c1.setArtista(rs.getString("artista"));
+                c1.setGenero(rs.getString("genero"));
+                listaCanciones.add(c1);
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            CRUD.cerrarConexion();
+        }
+
+        return listaCanciones;
+    }
+
+}
---
+++ Proyecto con GUI/Music/src/main/java/Modelo/Persistencia/CRUD.java
@@ -0,0 +1,126 @@
+package Modelo.Persistencia;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author JUAN MARIÑO
+ */
+public abstract class CRUD {
+
+    public static Connection con;
+    public static Statement stmt = null;
+    public static ResultSet rs = null;
+    
+    //Asigna la conexión
+    public static Connection setConnection(Connection connection) {
+        CRUD.con = connection;
+        return connection;
+    }
+
+    //Retornar la conexin
+    public static Connection getConnection() {
+        return con;
+    }
+
+    //Cerrar la conexin
+    public static void closeConnection(Connection con) {
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(CRUD.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    // Metodo que devuelve un ResultSet de una consulta (tratamiento de SELECT)
+    public static ResultSet consultarBD(String sentencia) {
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(sentencia);
+        } catch (SQLException sqlex) {
+            System.out.println(sqlex.getMessage());
+        } catch (RuntimeException rex) {
+            System.out.println(rex.getMessage());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return rs;
+    }
+
+    // Metodo que realiza un INSERT y devuelve TRUE si la operacin fue existosa
+    public static boolean insertarBD(String sentencia) {
+        try {
+            stmt = con.createStatement();
+            stmt.execute(sentencia);
+        } catch (SQLException | RuntimeException sqlex) {
+            System.out.println("ERROR RUTINA: " + sqlex);
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean borrarBD(String sentencia) {
+        try {
+            stmt = con.createStatement();
+            stmt.execute(sentencia);
+        } catch (SQLException | RuntimeException sqlex) {
+            System.out.println("ERROR RUTINA: " + sqlex);
+            return false;
+        }
+        return true;
+    }
+
+    // Mtodo que realiza una operacin como UPDATE, DELETE, CREATE TABLE, entre otras
+    // y devuelve TRUE si la operacin fue existosa
+    public static boolean actualizarBD(String sentencia) {
+        try {
+            stmt = con.createStatement();
+            stmt.executeUpdate(sentencia);
+        } catch (SQLException | RuntimeException sqlex) {
+            System.out.println("ERROR RUTINA: " + sqlex);
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean setAutoCommitBD(boolean parametro) {
+        try {
+            con.setAutoCommit(parametro);
+        } catch (SQLException sqlex) {
+            System.out.println("Error al configurar el autoCommit " + sqlex.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public static void cerrarConexion() {
+        closeConnection(con);
+    }
+
+    public static boolean commitBD() {
+        try {
+            con.commit();
+            return true;
+        } catch (SQLException sqlex) {
+            System.out.println("Error al hacer commit " + sqlex.getMessage());
+            return false;
+        }
+    }
+
+    public static boolean rollbackBD() {
+        try {
+            con.rollback();
+            return true;
+        } catch (SQLException sqlex) {
+            System.out.println("Error al hacer rollback " + sqlex.getMessage());
+            return false;
+        }
+    }
+}
